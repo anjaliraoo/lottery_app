719c2aacc99c0b97526066b4157cd723e21816ea
anjaliraoo <anjaliraoo1010@gmail.com>
2023-03-22 12:14:15 +0530

    added screens

--- lib/main.dart
+++ lib/main.dart
@@ -4,112 +4,98 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool flag = true;
+  int x = 0;
+  int lotnumber = 0;
+  final myController = TextEditingController();
+  String lot = 'Try to guess the Lottery number :))';
+  String trying = 'Let\'s try our luck';
+
   @override
   Widget build(BuildContext context) {
+    String text = myController.text;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Lottery App'),
+        ),
+        body: Column(children: [
+          Center(
+            child: Text(
+              '$lot',
+              style: const TextStyle(fontSize: 25),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              controller: myController,
+              enabled: flag,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.money),
+                labelText: 'Lottery Number',
+                hintText: 'Enter Your Lottery Number ',
+                fillColor: Colors.blue,
+              ),
             ),
-          ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.3),
+              ),
+              child: Visibility(
+                visible: text == '25011950',
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Hurray \nYOU WON the lottery'),
+                    Image.network(
+                      'https://justmaths.co.uk/wp-content/uploads/2016/10/celebration-gif.gif',
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                replacement: Text(trying),
+              ))
+        ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (x == 1) {
+              trying =
+                  'Aww you missed it \n so let me give you an hint \n it\'s an important date in January';
+            }
+            if (x == 2) {
+              trying =
+                  'OOps!! you missed it again \n so let me give you another hint \n it\'s the day when our constitution came into effect';
+            }
+            if (x == 3) {
+              trying =
+                  'Aww you tried very hard \n so let me give you another hint \n 3 years after the independence';
+            } else {
+              trying = 'Try and try but don\'t cry ';
+            }
+            x++;
+            setState(() {});
+          },
+          child: const Icon(Icons.check),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
